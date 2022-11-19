36ef4e7c1d9c8a797ec32b7d3d2b48625aef1e8f
hackisonjd <jackson@heehoposting.xyz>
2022-11-19 00:10:54 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("hello world")
+import os
+from dotenv import load_dotenv
+
+# Import the Discord library
+
+import discord
+
+dotenv_path = '.env'
+load_dotenv(dotenv_path)
+
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+
+# Create an instance of a client, which acts as a connection to Discord. Turning intents on is required for the bot to work.
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(DISCORD_TOKEN)
