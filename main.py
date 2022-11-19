3df9ec7db891f30a0bda8b0be4ab20a4b1ed40ca
hackisonjd <jackson@heehoposting.xyz>
2022-11-19 00:26:59 -0500

    add requirements.txt

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 import discord
 
+# Loads environment variables from .env file. DO NOT CHANGE THIS LINE AND DO NOT COMMIT .env FILE.
+
 dotenv_path = '.env'
 load_dotenv(dotenv_path)
 
@@ -17,16 +19,24 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+# NOTE: In order to add any future commands, you must precede them with a @client.event and an async def. More information can be found here: https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html
+
 @client.event
+# Simple async, which prints to the console when the bot is ready.
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
 @client.event
+# Prints to the console whenever a message is sent.
 async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+# TODO: ADD A DICE ROLLING FUNCTIONALITY HERE
+
+# Run the client. DO NOT CHANGE THIS LINE.
 
 client.run(DISCORD_TOKEN)
